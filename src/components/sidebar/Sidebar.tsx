262f54b09491238eab0f96d591bd5de26afa0ec1
{tanihirokun} <63832221+tanihirokun@users.noreply.github.com>
2022-03-02 12:23:53 +0900

    first commit

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import { memo, VFC } from "react"
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+
+export const SideBar: VFC = memo(() => {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+      <TwitterIcon className="sidebar__twittericon"/>
+
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+
+      {/* ツイートボタン */}
+    </div>
+  )
+})
